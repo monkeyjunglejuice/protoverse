cfbf7a8007521c2a5beffa131089072d7a99d79a
Dan Dee <monkeyjunglejuice@pm.me>
2025-04-08 16:50:19 +0800

    Update versions and deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Protoverse.MixProject do
     [
       app: :protoverse,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,6 +24,8 @@ defmodule Protoverse.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 end
